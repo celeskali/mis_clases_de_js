7af8fd5176a2f5e696a86e8fd1d3f6c6879f5386
Celeskali <celeskali@hotmail.com>
2023-03-11 20:38:58 -0300

    Se inició la subida del proyecto

---
+++ claseunojs.js
@@ -0,0 +1,21 @@
+// COMENTARIOS
+
+// EN LINEA
+/*
+EN BLOQUE
+*/
+
+
+// var nombre = "celeste" // NO SE RECOMIENDA
+// let apellido = "kaliniec" // SI SE RECOMIENDA LET
+// const edad = 22 // SI NO ES STRING VA SIN "" 
+
+// CONST = CONSTANTE, LO QUE SE USE CON CONST NO VA A CAMBIAR
+// EJEMPLO
+
+// const pi = 3.14
+
+let nombre = prompt("Ingrese su nombre")
+let apellido = prompt("Ingrese su apellido")
+let resultado = "¡Hola " + nombre + " " + apellido + "!"
+alert(resultado)
